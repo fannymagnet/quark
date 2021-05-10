675bbc722cc6c5b277d5810f712daa9043ae6976
vsensu <vsensu@foxmail.com>
2021-05-10 18:14:10 +0800

    test quark

--- test/xmake.lua
+++ test/xmake.lua
@@ -2,5 +2,7 @@ add_requires("catch2")
 
 target("test")
     set_kind("binary")
+    add_deps("quark-base")
+    add_includedirs("$(projectdir)/src")
     add_files("test.cpp")
     add_packages("catch2")
